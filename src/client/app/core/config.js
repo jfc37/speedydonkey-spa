d7a2e53f7202098cd7611a1388224ddd10cf02df
Joe <placid.joe@gmail.com>
2014-11-01 15:14:27 +1300

    Project setup

---
+++ src/client/app/core/config.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+
+    var core = angular.module('app.core');
+
+    core.config(toastrConfig);
+
+    /* @ngInject */
+    function toastrConfig(toastr){
+        toastr.options.timeOut = 4000;
+        toastr.options.positionClass = 'toast-bottom-right';
+    }
+
+    var config = {
+        appErrorPrefix: '[StudyBuddy Error] ', //Configure the exceptionHandler decorator
+        appTitle: 'Study Buddy',
+        version: '3.0.0'
+    };
+
+    core.value('config', config);
+
+    core.config(configure);
+
+    /* @ngInject */
+    function configure ($logProvider, $routeProvider, routehelperConfigProvider, exceptionConfigProvider) {
+        // turn debugging off/on (no info or warn)
+        if ($logProvider.debugEnabled) {
+            $logProvider.debugEnabled(true);
+        }
+
+        // Configure the common route provider
+        routehelperConfigProvider.config.$routeProvider = $routeProvider;
+        routehelperConfigProvider.config.docTitle = 'StudyBuddy: ';
+
+        // Configure the common exception handler
+        exceptionConfigProvider.config.appErrorPrefix = config.appErrorPrefix;
+    }
+
+})();
