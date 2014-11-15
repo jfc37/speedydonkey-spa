a40f3ff90e9eb6fc27a0d646fcacc6684641608e
Joe <placid.joe@gmail.com>
2014-11-15 13:24:04 +1300

    Made role a drop down item

--- src/client/app/register/registerPerson.js
+++ src/client/app/register/registerPerson.js
@@ -14,11 +14,21 @@
 
         vm.title = 'Register Person';
         vm.person = {};
+        vm.roles = [
+            {
+                display: 'Student',
+                value: 'student'
+            },
+            {
+                display: 'Professor',
+                value: 'professor'
+            },
+        ];
 
         vm.register = function() {
             return dataCreateService.createPerson(vm.person).then(function(response) {
                 if (response.is_valid){
-                    authService.setUserIdentityProperty('role', vm.person.role);
+                    authService.setUserIdentityProperty('role', response.action_result.role);
                     routehelper.redirectToRoute('dashboard');
                 } else{
                     logger.warning("Register failed");
