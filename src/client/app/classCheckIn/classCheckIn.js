4cd5214327be7c9656b646ddf4a7708cd955a7fb
Joe <placid.joe@gmail.com>
2015-04-28 17:28:49 +1200

    Change teachers from class check in

--- src/client/app/classCheckIn/classCheckIn.js
+++ src/client/app/classCheckIn/classCheckIn.js
@@ -5,10 +5,10 @@
         .module('app.classCheckIn')
         .controller('ClassCheckIn', ClassCheckIn);
 
-    ClassCheckIn.$inject = ['$q', 'classCheckInService', 'registerUserService', 'blockEnrolmentService', 'logger', 'validationService'];
+    ClassCheckIn.$inject = ['$q', 'classCheckInService', 'registerUserService', 'blockEnrolmentService', 'logger', 'validationService', 'manageClassesService'];
 
     /* @ngInject */
-    function ClassCheckIn($q, classCheckInService, registerUserService, blockEnrolmentService, logger, validationService) {
+    function ClassCheckIn($q, classCheckInService, registerUserService, blockEnrolmentService, logger, validationService, manageClassesService) {
         /*jshint validthis: true */
         var vm = this;
         vm.class = null;
@@ -17,6 +17,21 @@
         vm.areRegisteredStudentsLoading = true;
         vm.creatingNewAccount = false;
         vm.newUser = {};
+
+        vm.updateTeachers = function() {
+            var sanitisedClass = {
+                id: vm.class.id,
+                teachers: vm.class.teachers,
+                start_time: vm.class.start_time,
+                end_time: vm.class.end_time,
+                name: vm.class.name,
+            };
+            manageClassesService.update(sanitisedClass).then(function (){
+                logger.success('Teachers updated');
+            }, function(errors) {
+                logger.error('Problem updating teachers');
+            });
+        };
 
         vm.attendenceStatusChanged = function(student) {
             classCheckInService.attendenceStatusChanged(student).then(function(message) {
