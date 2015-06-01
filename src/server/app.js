e8e377f637e031816ed1fdfb36db43fe38a7a09c
Joe <placid.joe@gmail.com>
2015-06-01 18:49:36 +1200

    fix

--- src/server/app.js
+++ src/server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-.use(compress());
+app.use(compress());
 app.use(logger('dev'));
 app.use(cors());
 //app.use(errorHandler.init);
