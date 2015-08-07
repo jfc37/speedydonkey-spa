17c4a50a9666febb8e629b214937fc0cec55072c
JoeC <placid.joe@gmail.com>
2015-08-07 23:54:12 +1200

    Added Poli logos to payment directive

--- src/client/app/core/directives/onlinePayments/paymentOptionSelection.directive.js
+++ src/client/app/core/directives/onlinePayments/paymentOptionSelection.directive.js
@@ -16,3 +16,45 @@
         return directive;
     }
 })();
+
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .directive('paymentOptions', paymentOptions);
+
+    function paymentOptions(paypalExpressCheckout, poliPayment, logger) {
+        var directive = {
+            templateUrl: 'app/core/directives/onlinePayments/paymentOption.html',
+            require: ['ngModel'],
+            scope: {
+                ngModel: '=',
+                paymentConfig: '='
+            },
+            link: function (scope, element, attrs) {
+                scope.beginPaypalPayment = function () {
+                    paypalExpressCheckout.beginGeneric(scope.paymentConfig).then(function (paypalUrl) {
+                        window.location = paypalUrl;
+                    }, function () {
+                        paymentMethodError('PayPal');
+                    });
+                };
+
+                scope.beginPoliPayment = function () {
+                    poliPayment.begin(scope.paymentConfig).then(function (poliUrl) {
+                        window.location = poliUrl;
+                    }, function () {
+                        paymentMethodError('Poli');
+                    });
+                };
+
+                function paymentMethodError(paymentMethod) {
+                    logger.error('Looks like there\'s an issue with ' + paymentMethod + '. Try another payment method');
+                }
+            }
+        };
+        return directive;
+    }
+})();
