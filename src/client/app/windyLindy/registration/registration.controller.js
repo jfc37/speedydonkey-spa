3a9dce3e40b076d81d0dee35600854540108e3fb
JoeC <placid.joe@gmail.com>
2015-08-10 22:20:26 +1200

    Added all the required registration fields

--- src/client/app/windyLindy/registration/registration.controller.js
+++ src/client/app/windyLindy/registration/registration.controller.js
@@ -14,6 +14,38 @@
 
         vm.danceRoles = selectOptionService.getDanceRoles();
         vm.danceLevels = selectOptionService.getDanceLevels();
+        vm.classes = [
+            'Saturday 10:00 - 11:15',
+            'Saturday 11:30 - 12:45',
+            'Saturday 14:00 - 15:15',
+            'Saturday 15:30 - 16:45',
+            'Sunday 10:00 - 11:15',
+            'Sunday 11:30 - 12:45',
+            'Sunday 14:00 - 15:15',
+            'Monday 10:00 - 11:15',
+            'Monday 11:30 - 12:45',
+            'Monday 14:00 - 15:15',
+            'Monday 15:30 - 16:45'
+        ];
+        vm.socialEvents = [
+            {
+                name: 'Special Event',
+                price: 10.99
+            },
+            {
+                name: 'Love Making',
+                price: 1.99
+            },
+            {
+                name: 'The Michael Special',
+                price: 0.99
+            }
+        ];
+        vm.registration = {
+            classes: [],
+            events: [],
+            competitions: []
+        };
 
         vm.submit = function (form) {
             if (validationService.passesValidationCheck(form)) {
@@ -23,6 +55,20 @@
                     });
                 });
             }
-        }
+        };
+
+        vm.toggleSelection = function (itemArray, item) {
+            var idx = itemArray.indexOf(item);
+
+            // is currently selected
+            if (idx > -1) {
+                itemArray.splice(idx, 1);
+            }
+
+            // is newly selected
+            else {
+                itemArray.push(item);
+            }
+        };
     }
 })();
