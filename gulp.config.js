a54c06666aa824e5987f74984111e197bccdf29b
JoeC <placid.joe@gmail.com>
2015-09-20 18:47:12 +1200

    Added first set of tests for login controller

--- gulp.config.js
+++ gulp.config.js
@@ -93,8 +93,7 @@ module.exports = function () {
             'node_modules/chai/chai.js',
             'node_modules/mocha-clean/index.js',
             'node_modules/sinon-chai/lib/sinon-chai.js',
-            'node_modules/sinon/lib/sinon.js',
-            'node_modules/bardjs/dist/bard.js',
+            'node_modules/sinon/pkg/sinon.js'
         ],
         specs: [clientApp + '**/*.spec.js'],
 
