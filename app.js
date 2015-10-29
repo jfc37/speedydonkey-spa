6e0e37aac19cb768fc556529f177319e63d94028
JoeC <placid.joe@gmail.com>
2015-10-29 23:32:12 +1300

    Adding more security headers

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var cors = require('cors');
 
 var hsts = require('hsts');
 var csp = require('helmet-csp');
+var nosniff = require('dont-sniff-mimetype');
+var frameguard = require('frameguard');
+var ienoopen = require('ienoopen');
+var xssFilter = require('x-xss-protection');
 
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -27,6 +31,18 @@ app.use(cors());
 
 //Remove X-Powered-By header
 app.disable('x-powered-by');
+
+//Set X-Content-Type-Options header to no-sniff
+app.use(nosniff());
+
+//Set X-Frame-Options to stop site being put in any frame
+app.use(frameguard('deny'));
+
+//Set X-Download-Options header
+app.use(ienoopen());
+
+//Set X-XSS-Protection header
+app.use(xssFilter());
 
 //Add Strict-Transport-Security header to force https
 app.use(hsts({
@@ -48,7 +64,7 @@ app.use(csp({
     fontSrc: ["'self'", 'https://fonts.gstatic.com'],
     imgSrc: ["'self", 'https://www.google-analytics.com', 'data:'],
     styleSrc: ["'self'", "'unsafe-inline", 'https://fonts.googleapis.com'],
-    connectSrc: ["'self'", apiUrl],
+    connectSrc: ["'self'", apiUrl, 'https://cdn.raygun.io'],
     reportUri: 'https://report-uri.io/report/cb45e022bf5061dd8d8fc15e2abdad4e'
 }));
 
