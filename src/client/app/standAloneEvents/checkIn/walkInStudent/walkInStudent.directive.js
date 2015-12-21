e8af5132f90e5f9379e219564835e57bf64f46d7
JoeC <placid.joe@gmail.com>
2015-12-21 21:24:55 +1300

    adding event check in widget to dashboard

--- src/client/app/standAloneEvents/checkIn/walkInStudent/walkInStudent.directive.js
+++ src/client/app/standAloneEvents/checkIn/walkInStudent/walkInStudent.directive.js
@@ -6,7 +6,7 @@
         .directive('walkInStudent', walkInStudent);
 
     /* @ngInject */
-    function walkInStudent() {
+    function walkInStudent(registerUserService, validationService) {
         return {
             restrict: 'E',
             bindToController: true,
@@ -20,11 +20,35 @@
                 var vm = this;
 
                 vm.addWalkIn = function () {
-                    vm.students.push({
-                        id: vm.selectedStudent.id
-                    });
+                    vm.students.push(angular.copy(vm.selectedStudent));
 
                     vm.selectedStudent = undefined;
+                };
+
+                vm.createAccount = function () {
+                    vm.newUser = {};
+                    if (vm.selectedStudent) {
+                        var splitName = vm.selectedStudent.split(' ');
+                        if (splitName.length > 0) {
+                            vm.newUser.firstName = splitName[0];
+                        }
+                        if (splitName.length > 1) {
+                            vm.newUser.surname = splitName[1];
+                        }
+                    }
+
+                    vm.creatingNewAccount = true;
+                };
+
+                vm.registerNewUser = function (form) {
+                    registerUserService.register(vm.newUser, true).then(function (user) {
+                        vm.creatingNewAccount = false;
+                        vm.selectedStudent = user;
+
+                        vm.addWalkIn();
+                    }, function (validationErrors) {
+                        validationService.applyServerSideErrors(form, validationErrors);
+                    });
                 };
             }
         };
